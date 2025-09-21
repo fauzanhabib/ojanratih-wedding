7b836f01f96a8ef883b71b02d22456b97d2a3acb
Fauzan <fauzanhabib20@gmail.com>
2025-09-21 09:20:52 +0700

    update typo

--- app/Akad.tsx
+++ app/Akad.tsx
@@ -143,7 +143,7 @@ const CoupleSection: React.FC = () => (
         <h3 className="font-playfair text-base sm:text-2xl text-brand-dark">S.Ak M.Ak</h3>
         <p className="font-lato text-brand-dark text-xs sm:text-sm mt-2">Putri Bungsu dari</p>
         <p className="font-lato text-brand-dark text-xs sm:text-sm">
-          Bapak H. Wawan Ridwan Wahyu, S.Ap (Purnabakti Dishub Pandeglang) <br /> & Ibu Hj. Suryati, S Ag (Hj.Yati) (Punabakti Kemenag/KTU MAN 2 Pandeglang)
+          Bapak H. Wawan Ridwan Wahyu, S.Ap (Purnabakti Dishub Pandeglang) <br /> & Ibu Hj. Suryati, S Ag (Hj.Yati) (Purnabakti Kemenag/KTU MAN 2 Pandeglang)
         </p>
         <a
           href="#"
@@ -354,7 +354,7 @@ const CountdownSection = () => {
             Fathullah, S.Ag., M.M., M.H., CPM / Dewi Nuraeni (Paman / Bibi – Cilegon)
           </p>
           <p className="font-lato font-semibold text-base sm:text-lg md:text-xl text-black">
-            Hasanudin (Udin) -  Noneng Komala, S.Pd.I. (Paman / Bibi – Ciekek Malati Pandeglang) 
+            Hasanudin (Udin) - Noneng Komala, S.Pd.I. (Paman / Bibi – Ciekek Malati Pandeglang) 
           </p>
         </div>
       </div>
